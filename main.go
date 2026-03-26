803d273f0a17124e5b2ec381457ebaaf8f89645a
treytuscai <ttuscai22@icloud.com>
2026-03-26 00:52:53 +0000

    feat: add tabwriter output and --watch live refresh

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/treytuscai/meshwatch/internal/discovery"
 	"log"
+	"os"
+	"text/tabwriter"
+	"time"
 )
 
 func main() {
+	watch := flag.Bool("watch", false, "continuously refresh")
+	flag.Parse()
+
+	for {
+		scan()
+		if !*watch {
+			break
+		}
+		time.Sleep(2 * time.Second)
+		fmt.Print("\033[H\033[2J") // clear screen
+	}
+}
+
+func scan() {
 	entries, err := discovery.ParseProcNetTCP()
 	if err != nil {
 		log.Fatal(err)
@@ -15,17 +33,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	fmt.Fprintf(w, "LOCAL\tREMOTE\tSTATE\tPROCESS\n")
 	for _, e := range entries {
 		pid := inodeMap[e.Inode]
 		info, err := discovery.GetProcessInfo(pid)
 		if err != nil {
 			continue
 		}
-		fmt.Printf("%-21s %-21s %-13s %s\n",
-			fmt.Sprintf("%s:%d", e.LocalIP, e.LocalPort),
-			fmt.Sprintf("%s:%d", e.RemoteIP, e.RemotePort),
+		fmt.Fprintf(w, "%s:%d\t%s:%d\t%s\t%s\n",
+			e.LocalIP, e.LocalPort,
+			e.RemoteIP, e.RemotePort,
 			e.State,
 			info.Name,
 		)
 	}
+	w.Flush()
 }
